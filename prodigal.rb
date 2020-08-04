66cc1c9b8fb2028c4db6f82b529df937e8212c5e
Logan Thomas <thomas.lo@husky.neu.edu>
2020-08-04 13:34:59 -0700

    Update prodigal.rb

--- prodigal.rb
+++ prodigal.rb
@@ -3,7 +3,7 @@ require "formula"
 class Prodigal < Formula
   homepage ""
   url "https://github.com/hyattpd/Prodigal/archive/v2.6.2.tar.gz"
-  sha1 "ce149f600505feed1ed0d4f3c3cfd5d831379187"
+  sha256 "ce149f600505feed1ed0d4f3c3cfd5d831379187"
 
   def install
     system "make", "install"
